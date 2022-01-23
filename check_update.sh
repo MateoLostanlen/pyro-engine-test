5047fd65b0dfd67d1eb87160656aa20d69e77f05
Mateo LOSTANLEN <mateo.lostanlen@gmail.com>
2022-01-23 12:33:53 +0100

    update script

--- check_update.sh
+++ check_update.sh
@@ -3,14 +3,16 @@
 
 set -u
 echo $(date +"%H:%M:%S") >> '/home/pi/git.txt';
+git -C /home/pi/pyro-engine-test reset --hard;
+git -C /home/pi/pyro-engine-test fetch origin master;
 
-git fetch origin master;
-
-PING_CMD=('git' 'diff' 'origin/master')
+PING_CMD=('git' '-C' '/home/pi/pyro-engine-test' 'diff' 'origin/master')
 
 if "${PING_CMD[@]}";
 then
-    echo 'ok' >> '/home/pi/git.txt';
+    echo 'uptodate' >> '/home/pi/git.txt';
+    exit 0;
 fi;
 
-echo 'ko' >> '/home/pi/git.txt';
+echo 'updating from repo ...' >> '/home/pi/git.txt';
+git -C /home/pi/pyro-engine-test merge origin/master;
