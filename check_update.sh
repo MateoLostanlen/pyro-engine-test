0aa632de0b3e79a8e190873108ff50ebfff52195
Mateo LOSTANLEN <mateo.lostanlen@gmail.com>
2022-01-23 12:35:58 +0100

    print full date

--- check_update.sh
+++ check_update.sh
@@ -2,7 +2,7 @@
 
 
 set -u
-echo $(date +"%H:%M:%S") >> '/home/pi/git.txt';
+echo $(date -u) >> '/home/pi/git.txt';
 git -C /home/pi/pyro-engine-test reset --hard;
 git -C /home/pi/pyro-engine-test fetch origin master;
 
